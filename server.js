875ce0aea5b75dd23a83df3bfca37c8bc437f8e7
Aaron Smith <aaron.smith@aspect.com>
2016-04-15 03:14:14 -0400

    tweaks

--- server.js
+++ server.js
@@ -6,16 +6,15 @@ var app = express();
 var connection = mysql.createConnection({
 	host		: 'helloworlduseast1.cyjsfa15axuh.us-east-1.rds.amazonaws.com',
 	user		: 'hwadmin',
-	password	: '..Wide..Open'
+	password	: '..Wide..Open',
+	port		: '3306'
 });
-
-
 
 app.get('/', function (req, res) {
 	res.send('Hello World!');
 	connection.query('SELECT first_name,last_name from helloworld.employees ORDER BY RAND() LIMIT 1', function(err, rows, fields) {
 		if (err) throw err;
-		console.log('My name is ', rows[0].solution);
+		res.send('My name is ', rows[0]);
 	});
 });
 
