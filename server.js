3c2314021d71f9fadf99a2aa11234e5b771c9941
Aaron Smith <aaron.smith@aspect.com>
2016-04-15 09:20:28 -0400

    troubleshooting

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ var connection = mysql.createConnection({
 app.get('/', function (req, res) {
 	//res.send('Hello World!');
 	connection.query('SELECT first_name,last_name,position,salary from helloworld.employees ORDER BY RAND() LIMIT 1', function(err, rows, fields) {
-		var json_line = rows.substring(1, rows.length-1);
+		var rows = rows.substring(1, rows.length-1);
 		//var line = JSON.parse(json_line);
 		if (!err)
 			//res.send('My name is ', alert(line['first_name']), alert(line['last_name']), ' and I make ',  alert(line['salary']), ' as a ', alert(line['position']));
-			res.send('My name is ', json_line);
+			res.send('My name is ', rows);
 		else
 			res.send('Error while performing Query.');
 	});
